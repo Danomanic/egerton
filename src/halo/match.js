b76d4526b46180143665030b255d580f34e44156
Daniel Pomfret <dan@coly.uk>
2021-12-30 09:32:51 +0000

    Fix codesmell

--- src/halo/match.js
+++ src/halo/match.js
@@ -8,9 +8,9 @@ const db = require('monk')(config.DB_URI);
 
 const matches = db.get('matches');
 
-const sendMatchEmbed = async (match, channel, gamerTag) => {
-	const gamerTagStats = await getGamerTagStats(gamerTag, match.players);
-	const allGuildies = await getAllGuildies(match.players);
+const sendMatchEmbed = async (matchData, channel, gamerTag) => {
+	const gamerTagStats = await getGamerTagStats(gamerTag, matchData.players);
+	const allGuildies = await getAllGuildies(matchData.players);
 	const embedBuilder = new MessageEmbed()
 		.setColor(getOutcomeColour(gamerTagStats.outcome))
 		.setTitle('A member of our Team has played a match!')
@@ -20,17 +20,17 @@ const sendMatchEmbed = async (match, channel, gamerTag) => {
 			{ name: 'Result', value: `${gamerTagStats.outcome.toUpperCase()}`, inline: true },
 			{ name: 'Guildies', value: `${allGuildies}`, inline: true },
 			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Map', value: `${match.details.map.name} (${match.details.playlist.name})`, inline: true },
-			{ name: 'Gametype', value: match.details.category.name, inline: true },
-			{ name: 'Duration', value: match.duration.human, inline: true },
+			{ name: 'Map', value: `${matchData.details.map.name} (${matchData.details.playlist.name})`, inline: true },
+			{ name: 'Gametype', value: matchData.details.category.name, inline: true },
+			{ name: 'Duration', value: matchData.duration.human, inline: true },
 			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Teams', value: teamResultsTable(match.teams.details) },
-			{ name: '\u200B', value: playerResultsTable(match.players, 0) },
-			{ name: '\u200B', value: playerResultsTable(match.players, 1) },
+			{ name: 'Teams', value: teamResultsTable(matchData.teams.details) },
+			{ name: '\u200B', value: playerResultsTable(matchData.players, 0) },
+			{ name: '\u200B', value: playerResultsTable(matchData.players, 1) },
 		)
-		.setURL('https://tracker.gg/halo-infinite/match/' + match.id)
-		.addField('Tracker.gg Link', 'https://tracker.gg/halo-infinite/match/' + match.id)
-		.setTimestamp(new Date(match.played_at));
+		.setURL('https://tracker.gg/halo-infinite/match/' + matchData.id)
+		.addField('Tracker.gg Link', 'https://tracker.gg/halo-infinite/match/' + matchData.id)
+		.setTimestamp(new Date(matchData.played_at));
 
 	channel.send({ embeds: [embedBuilder] });
 };
