e6bbb7b11bf14fe778b878f28d6e38a9210dc317
Daniel Pomfret <dan@coly.uk>
2022-01-09 18:18:27 +0000

    Add error handling!

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -11,15 +11,27 @@ module.exports = {
 		.addStringOption(option => option.setName('gamertag').setDescription('The Players Xbox Gamertag')),
 	async execute(interaction) {
 		await interaction.deferReply();
-		const gamertag = await interaction.options.getString('gamertag');
-		const genUuid = uuid.v1();
-		await axios.get(`${config.EGERTON_IMAGES_API}/generate/gamertag/${gamertag}/${genUuid}`);
-		const embedBuilder = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle(`${gamertag}'s Stats`)
-			.setAuthor({ name: 'Egerton', iconURL: 'https://i.imgur.com/YEjKMuZ.png' })
-			.setDescription(`Here are the stats for ${gamertag}`)
-			.setImage(`${config.CLOUDFRONT_DOMAIN}/gamertag/${genUuid}.png`);
-		await interaction.editReply({ embeds: [embedBuilder] });
+		try {
+			const gamertag = await interaction.options.getString('gamertag');
+			const genUuid = uuid.v1();
+			const repsonse = await axios.get(`${config.EGERTON_IMAGES_API}/generate/gamertag/${gamertag}/${genUuid}`);
+			console.log(repsonse);
+
+			if (repsonse.status === 200) {
+				const embedBuilder = new MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle(`${gamertag}'s Stats`)
+					.setAuthor({ name: 'Egerton', iconURL: 'https://i.imgur.com/YEjKMuZ.png' })
+					.setDescription(`Here are the stats for ${gamertag}`)
+					.setImage(`${config.CLOUDFRONT_DOMAIN}/gamertag/${genUuid}.png`);
+				await interaction.editReply({ embeds: [embedBuilder] });
+			}
+			else {
+				await interaction.editReply('Gamertag does not exist. Is the Gamertag correct?');
+			}
+		}
+		catch (err) {
+			await interaction.editReply('Something went wrong, please try again later.');
+		}
 	},
 };
