dd5a97d54f522cc51bbf4f5baa2eba80bf7e2132
Daniel Pomfret <dan@coly.uk>
2022-01-09 01:17:29 +0000

    Fix egerton thinking...

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,8 +1,10 @@
 const config = require('../config');
-const { MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const uuid = require('uuid');
 const axios = require('axios');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,17 +12,17 @@ module.exports = {
 		.setDescription('Replies with your Halo Infinite stats.')
 		.addStringOption(option => option.setName('gamertag').setDescription('The Players Xbox Gamertag')),
 	async execute(interaction) {
+		await interaction.deferReply();
 		const gamertag = await interaction.options.getString('gamertag');
 		const genUuid = uuid.v1();
 		await axios.get(`${config.EGERTON_IMAGES_API}/generate/gamertag/${gamertag}/${genUuid}`);
-
+		console.log(`${config.CLOUDFRONT_DOMAIN}/gamertag/${genUuid}.png`);
 		const embedBuilder = new MessageEmbed()
 			.setColor('#0099ff')
 			.setTitle(`${gamertag}'s Stats`)
 			.setAuthor({ name: 'Egerton', iconURL: 'https://i.imgur.com/YEjKMuZ.png' })
 			.setDescription(`Here are the stats for ${gamertag}`)
-			.setImage(`${config.CLOUDFRONT_DOMAIN}/gamertag/{genUuid}.png`);
-
-		await interaction.reply({ embeds: [embedBuilder] });
+			.setImage(`${config.CLOUDFRONT_DOMAIN}/gamertag/${genUuid}.png`);
+		await interaction.editReply({ embeds: [embedBuilder] });
 	},
 };
