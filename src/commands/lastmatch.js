61b402070c277acc54a55a82fef7b063956cfd6f
Daniel Pomfret <dan@coly.uk>
2022-01-09 17:32:40 +0000

    Add last match command

--- src/commands/lastmatch.js
+++ src/commands/lastmatch.js
@@ -1,25 +1,26 @@
 const config = require('../config');
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const uuid = require('uuid');
+const Halo = require('../lib/halo');
 const axios = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('stats')
-		.setDescription('Replies with your Halo Infinite stats.')
+		.setName('lastmatch')
+		.setDescription('Get info on a Gamer Tags last match.')
 		.addStringOption(option => option.setName('gamertag').setDescription('The Players Xbox Gamertag')),
 	async execute(interaction) {
 		await interaction.deferReply();
 		const gamertag = await interaction.options.getString('gamertag');
-		const genUuid = uuid.v1();
-		await axios.get(`${config.EGERTON_IMAGES_API}/generate/gamertag/${gamertag}/${genUuid}`);
+		const lastMatch = await Halo.getLastMatch(gamertag);
+		await axios.get(`${config.EGERTON_IMAGES_API}/generate/match/${lastMatch.id}`);
+
 		const embedBuilder = new MessageEmbed()
 			.setColor('#0099ff')
-			.setTitle(`${gamertag}'s Stats`)
+			.setTitle(`${gamertag}'s Last Match Data`)
 			.setAuthor({ name: 'Egerton', iconURL: 'https://i.imgur.com/YEjKMuZ.png' })
-			.setDescription(`Here are the stats for ${gamertag}`)
-			.setImage(`${config.CLOUDFRONT_DOMAIN}/gamertag/${genUuid}.png`);
+			.setDescription(`Here are the stats for ${gamertag}'s last match`)
+			.setImage(`${config.CLOUDFRONT_DOMAIN}/match/${lastMatch.id}.png`);
 		await interaction.editReply({ embeds: [embedBuilder] });
 	},
 };
