7487bcb1d50559e066b3cb9e857e0c70b6b0ea71
Daniel Pomfret <dan@coly.uk>
2022-01-13 11:41:39 +0000

    Fix version issue

--- src/halo/elements/match-table.js
+++ src/halo/elements/match-table.js
@@ -1,29 +1,5 @@
 const AsciiTable = require('ascii-table');
 
-const playerResultsTable = (results, teamId) => {
-	const table = new AsciiTable();
-	table.setHeading('', 'S', 'K', 'D', 'A', 'Dmg', 'CSR');
-	results.forEach((playerDetails) => {
-		if (playerDetails.team.id === teamId) {
-			table.setTitle(playerDetails.team.name);
-			table.addRow(
-				`${trimString(playerDetails.gamertag, 10)}`,
-				playerDetails.stats.core.score,
-				`${playerDetails.stats.core.summary.kills}`,
-				`${playerDetails.stats.core.summary.deaths}`,
-				`${playerDetails.stats.core.summary.assists}`,
-				`${playerDetails.stats.core.damage.dealt}`,
-				`${playerDetails.progression.csr.post_match.value} [${getCSRDifference(playerDetails.progression.csr.pre_match.value, playerDetails.progression.csr.post_match.value)}]`,
-			);
-		}
-	});
-	table.sortColumn(1, function(a, b) {
-		return b - a;
-	});
-	table.removeBorder();
-	return '```\n{output}\n```'.replace('{output}', table.toString());
-};
-
 const teamResultsTable = (results) => {
 	const table = new AsciiTable();
 	table.setHeading('', '', 'S', 'K', 'D', 'A');
@@ -41,12 +17,4 @@ const teamResultsTable = (results) => {
 	return '```\n{output}\n```'.replace('{output}', table.toString());
 };
 
-const trimString = (string, length) => string.length > length ? string.substring(0, length - 3) + '.' : string;
-
-const getCSRDifference = (before, after) => {
-	const difference = after - before;
-
-	return (difference <= 0 ? '' : '+') + difference;
-};
-
-module.exports = { playerResultsTable, teamResultsTable };
+module.exports = { teamResultsTable };
--- src/lib/halo.js
+++ src/lib/halo.js
@@ -1,6 +1,6 @@
 const config = require('../config');
 const lib = require('lib')({ token: config.STDLIB_SECRET_TOKEN });
-const HaloAPI = lib.halo.infinite['@0.3.3'];
+const HaloAPI = lib.halo.infinite['@0.3.5'];
 
 const getLastMatch = async (gamertag) => {
 	const result = await HaloAPI.stats.matches.list({
