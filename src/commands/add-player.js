12fcd59f64cff81d2cb8e00332084feae7af3674
Daniel Pomfret <dan@coly.uk>
2022-01-14 12:42:01 +0000

    Refactor commands to standardise

--- src/commands/add-player.js
+++ src/commands/add-player.js
@@ -22,17 +22,17 @@ module.exports = {
 	async execute(interaction) {
 		const user = await interaction.user;
 		const gamerTag = await interaction.options.getString('gamertag').toLowerCase();
-		await interaction.reply({ content : `Hello <@${user.id}> - I'm just checking to see if the GamerTag ${gamerTag} exists...`, ephemeral: true });
+		await interaction.reply({ content : `Just checking to see if the GamerTag ${gamerTag} exists...`, ephemeral: true });
 		try {
 			const lastMatch = await Halo.getLastMatch(gamerTag);
 			if (lastMatch.id && !await players.findOne({ gamerTag })) {
 				await players.insert({ gamerTag, user: user.id, guildId: interaction.guildId, channelId: interaction.channelId });
-				await interaction.followUp({ content: `<@${user.id}> GamerTag exists! I've added ${gamerTag} to the Database to check for new matches.`, ephemeral: true });
+				await interaction.followUp({ content: `GamerTag exists! I've added ${gamerTag} to the Database to check for new matches.`, ephemeral: true });
 			}
 		}
 		catch (err) {
 			console.log(err);
-			await interaction.editReply(`<@${user.id}> Something went wrong while trying to add the GamerTag. Is the GamerTag correct?`);
+			await interaction.followUp({ content : 'Something went wrong while trying to add the GamerTag. Is the GamerTag correct?', ephemeral: true });
 		}
 	},
 };
--- src/commands/remove-player.js
+++ src/commands/remove-player.js
@@ -20,18 +20,18 @@ module.exports = {
 	],
 	async execute(interaction) {
 		const gamerTag = await interaction.options.getString('gamertag').toLowerCase();
-		await interaction.reply(`Removing ${gamerTag} from the Database...`);
+		await interaction.reply({ content: `Removing ${gamerTag} from the Database...`, ephemeral: true });
 		try {
 			if (await players.findOne({ gamerTag })) {
 				await players.remove({ gamerTag });
-				await interaction.followUp(`${gamerTag} removed from the Database.`);
+				await interaction.followUp({ content: `${gamerTag} removed from the Database.`, ephemeral: true });
 			}
 			else {
-				await interaction.followUp(`${gamerTag} is not in the Database. So I can't remove it.`);
+				await interaction.followUp({ content: `${gamerTag} is not in the Database. So I can't remove it.`, ephemeral: true });
 			}
 		}
 		catch (err) {
-			await interaction.followUp('Something went wrong while tring to remove the GamerTag. Please try again later.');
+			await interaction.followUp({ content: 'Something went wrong while tring to remove the GamerTag. Please try again later.', ephemeral: true });
 		}
 	},
 };
